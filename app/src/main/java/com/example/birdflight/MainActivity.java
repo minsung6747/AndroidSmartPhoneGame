414df07fbc6c903a793da75d5d9b3fec7f16ff49
minsung6747 <miin_sung@naver.com>
2024-05-06 12:29:12 +0900

    added Flight class

--- app/src/main/java/com/example/birdflight/MainActivity.java
+++ app/src/main/java/com/example/birdflight/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.birdflight;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
